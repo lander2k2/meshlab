426de0cd422ddee87caa81af50732c60fac17135
Richard Lander <lander2k2@protonmail.com>
2018-06-27 17:01:37 -0400

    Add rate-limited data service

--- user_interface/ui.go
+++ user_interface/ui.go
@@ -7,41 +7,44 @@ import (
 	"net/http"
 )
 
-type PageContent struct {
-	Data string
+type Service struct {
+	Name     string
+	Endpoint string
+	Response string
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
 
 	log.Println("Index page requested")
 
-	content := PageContent{}
+	services := []Service{
+		{Name: "LB Data", Endpoint: "http://meshlab-lb-data-svc/timedata"},
+		{Name: "Rate Limit Data", Endpoint: "http://meshlab-rate-limit-data-svc/timedata"},
+	}
 
-	// parse page template
 	tmpl, err := template.ParseFiles("/index.html")
 	if err != nil {
 		log.Println("Failed to parse template", err)
 	}
 
-	// call data service
-	resp, err := http.Get("http://meshlab-lb-data-svc/timedata")
-	if err != nil {
-		log.Println("Failed to get response from load balanced data service")
-
-		content.Data = "Nothing - it doesn't seem to be running"
-	}
-
-	if resp != nil {
-		data, err := ioutil.ReadAll(resp.Body)
+	for i, svc := range services {
+		resp, err := http.Get(svc.Endpoint)
 		if err != nil {
-			log.Println("Failed to read data service response")
-
-			content.Data = "Nothing intelligble - it seems to be broken"
+			log.Println("Failed to get response from load balanced data service")
+			services[i].Response = "Nothing - it doesn't seem to be running"
 		}
-		content.Data = string(data)
+
+		if resp != nil {
+			data, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Println("Failed to read data service response")
+				services[i].Response = "Nothing intelligble - it seems to be broken"
+			}
+			services[i].Response = string(data)
+		}
 	}
 
-	tmpl.Execute(w, content)
+	tmpl.Execute(w, services)
 
 	log.Println("Index page served")
 }
